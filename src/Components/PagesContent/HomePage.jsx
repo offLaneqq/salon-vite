44bfc964906054c121f8e2336e03d90e2e2de983
Dmytro <srk90512@gmail.com>
2023-08-28 04:28:32 +0300

    import Branding.jsx

--- src/Components/PagesContent/HomePage.jsx
+++ src/Components/PagesContent/HomePage.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import Home from "../Home/Home";
+import Branding from "../Home/Branding";
 
 const HomePage = () => {
   return (
     <div className="text-xl">
       <Home />
+      <Branding />
     </div>
   );
 };
