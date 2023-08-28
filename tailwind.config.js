8f5ee19f050aa1c4cba7f0e0b7b1549380f1e163
Dmytro <srk90512@gmail.com>
2023-08-28 20:07:19 +0300

    add colors, keyframes, anumation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header': '#A97964',
+        'light-brown': '#FDF0D9',
+        'purple-light': '#A06580',
+        'purple-dark': '#883B62'
+      },
+      keyframes: {
+        slide: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateY(50%)'
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateY(0)'
+          }
+        }
+      },
+      animation: {
+        slide: 'slide 1s ease-out forwards'
+      }
+    },
   },
   plugins: [],
 }
